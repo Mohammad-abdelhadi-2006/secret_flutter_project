f1ab0b4ecf6a8a7aba806a951f79a740ef63a470
Mohammad Abdelhadi <mohabdelhadi2006@gmail.com>
2025-11-23 19:29:51 +0300

    Start Flutter  Project

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: SimpleProject(),
+    );
+  }
+}
+
+class SimpleProject extends StatelessWidget {
+  const SimpleProject({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //action_button
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.access_time_filled)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.account_box)),
+        ],
+        elevation: 12,
+        backgroundColor: Colors.amberAccent,
+        title: Text("School APP"),
+        centerTitle: true,
+        //leading_button
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.abc_outlined),
+          color: Colors.blue,
+          iconSize: 30,
+        ),
+      ),
+      body: Text("Hello Flutter", style: TextStyle(fontSize: 40)),
+    );
+  }
+}
